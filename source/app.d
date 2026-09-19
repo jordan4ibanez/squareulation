e862d363b175028aad80c01fb73a8ca4ffa8ebe2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-09-19 01:41:27 -0400

    Oh I absolutely love that

--- source/app.d
+++ source/app.d
@@ -8,6 +8,7 @@ void main() {
 	auto map = Grid!int(5, 5);
 
 	SetTraceLogLevel(TraceLogLevel.LOG_WARNING);
+	SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
 
 	validateRaylibBinding();
 	InitWindow(800, 400, "Squareulation");
@@ -15,10 +16,22 @@ void main() {
 
 	Textures.load();
 
+	Camera2D camera;
+	camera.target = Vector2(0, 0);
+	camera.rotation = 0f;
+	camera.zoom = 2.0f;
+
 	while (!WindowShouldClose()) {
 
 		BeginDrawing();
 		ClearBackground(Colors.RAYWHITE);
+
+		BeginMode2D(camera);
+
+		auto windowWidth = GetScreenWidth();
+		auto windowHeight = GetScreenHeight();
+
+		camera.offset = Vector2(windowWidth / 2, windowHeight / 2);
 
 		foreach (x; 0 .. 10) {
 			foreach (y; 0 .. 10) {
@@ -29,6 +42,8 @@ void main() {
 				DrawTexturePro(texture, source, dest, Vector2(0, 0), 0, Colors.WHITE);
 			}
 		}
+
+		EndMode2D();
 
 		EndDrawing();
 	}
