26f97e5a313298e0c42a429ea4f4f358533a8bf9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-09-19 01:20:57 -0400

    whatever this is

--- source/app.d
+++ source/app.d
@@ -19,7 +19,17 @@ void main() {
 
 		BeginDrawing();
 		ClearBackground(Colors.RAYWHITE);
-		DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
+
+		foreach (x; 0 .. 10) {
+			foreach (y; 0 .. 10) {
+				// DrawTextureEx(Textures.get("arrow.png"), x * 32, y * 32, Colors.WHITE);
+				auto texture = Textures.get("arrow.png");
+				auto source = Rectangle(0, 0, texture.width, texture.height);
+				auto dest = Rectangle(x * 32, y * 32, 32, 32);
+				DrawTexturePro(texture, source, dest, Vector2(0, 0), 0, Colors.WHITE);
+			}
+		}
+
 		EndDrawing();
 	}
 
