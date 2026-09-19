20c3ff2e758d12635564283c0e1a5800c2444b4b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-09-19 00:59:35 -0400

    It works! Hooray!

--- source/textures/textures.d
+++ source/textures/textures.d
@@ -1,5 +1,7 @@
 module textures.textures;
 
+import core.sys.posix.libgen;
+import raylib;
 import std.file;
 import std.path;
 import std.stdio;
@@ -8,11 +10,25 @@ import std.string;
 final static class Textures {
 static:
 
+    Texture2D[string] database;
+
     void load() {
-        foreach (DirEntry entry; dirEntries(getcwd(), SpanMode.depth)) {
+
+        string rootDir = absolutePath(getcwd());
+
+        foreach (DirEntry entry; dirEntries(rootDir, SpanMode.depth)) {
             if (entry.isFile && entry.name.extension.toLower() == ".png") {
                 // todo: load file into memory, save png as something. However raylib does it.
-                writeln(entry.name);
+
+                string fullPath = entry.name;
+                string fileName = fullPath.baseName;
+
+                // This may be sloppy, but I don't want it to be that sloppy.
+                if (fileName in database) {
+                    throw new Exception(fileName ~ " is a duplicate! Hit in: " ~ fullPath);
+                } else {
+                    database[fileName] = LoadTexture(fullPath.toStringz);
+                }
             }
         }
     }
