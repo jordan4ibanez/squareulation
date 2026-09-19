0678826d147177f775bae44a133095189c9d811d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-09-18 23:34:55 -0400

    Very good

--- source/app.d
+++ source/app.d
@@ -1,11 +1,24 @@
+import raylib;
 import std.stdio;
 import utility.grid;
 
 void main() {
+
+	validateRaylibBinding();
+	InitWindow(800, 400, "Squareulation");
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) {
+
+		BeginDrawing();
+		ClearBackground(Colors.RAYWHITE);
+		DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
+		EndDrawing();
+	}
 
 	auto map = Grid!int(5, 5);
 
 	writeln("Edit source/app.d to start your project.");
 
 	writeln(map[4, 4]);
+	CloseWindow();
 }
